dd376b5f54106d413ea0c97f08e833e3239d655e
Matt Haberland <mhaberla@calpoly.edu>
2025-06-30 18:18:24 -0700

    fix: fix __version__

--- package1/src/package1/__init__.py
+++ package1/src/package1/__init__.py
@@ -1,5 +1,7 @@
 # package1/__init__.py (can be empty or contain initialization code)
 
-__version__ = "0.1.0"
 
+from importlib.metadata import version
 from package1._code import *  # noqa: F403
+
+__version__ = version("monorepo_package1")
